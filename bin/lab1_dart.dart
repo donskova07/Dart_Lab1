e41a023b6fdca49c4ffefc4ffd67359d0cbfec9f
donskova07 <rezermat@gmail.com>
2026-09-07 13:36:35 +0300

    change lab

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -1,5 +1,25 @@
-import 'package:lab1_dart/lab1_dart.dart' as lab1_dart;
+void main() {
+String name = "Артем";
+int age = 20;
+double height = 1.75;
+bool isStudent = true;
 
-void main(List<String> arguments) {
-  print('Hello world: ${lab1_dart.calculate()}!');
+print(name);
+print(age);
+print(height);
+print(isStudent);
+
+print('Привет, $name! тебе $age лет.');
+print('Через 5 лет тебе будет ${age + 5} лет.');
+print('Рост: ${height} м, студент: $isStudent');
+
+var score = 95;
+var language = "Dart";
+print("$language: $score");
+const String appName = "Lab1";
+final int startYear = 2026;
+print("$appName in $startYear");}
+String& city = null;
+if (city != null) {
+  print(city.toUpperCase(),);
 }
